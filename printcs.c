035028c72a25d5e0928a1f64555ef6d2d5d96051
Shimaa Salah <shimaasalah91.13@gmail.com>
2023-09-16 11:11:11 -0700

    printc string digit function

--- printcs.c
+++ printcs.c
@@ -9,37 +9,33 @@
  */
 int printcs(char format, va_list ptr, int counter)
 {
-switch (format)
-{
-case 'c':
-_putchar(va_arg(ptr, int));
-counter++;
-break;
-case 's':
-counter = printstring(ptr, counter);
-break;
-case '%':
-_putchar('%');
-counter++;
-break;
-case 'd':
-case 'i':
-counter = printdigit(ptr, counter);
-break;
-case 'b':
-case 'o':
-case 'x':
-case 'X':
-counter = printbase(format, ptr, counter);
-break;
-case 'u':
-counter = printunsign(ptr, counter);
-break;
-default:
-_putchar('%');
-_putchar(format);
-counter += 2;
-break;
+	char str;
+
+	if (format == 'c')
+	{
+		str = va_arg(ptr, int);
+		_putchar(str);
+		counter++;
+	}
+	else if (format == 's')
+		counter = printstring(ptr, counter);
+	else if (format == '%')
+	{
+		_putchar('%');
+		counter++;
+	}
+	else if ((format == 'd') || (format == 'i'))
+		counter = printdigit(ptr, counter);
+	else if (format == 'b' || format == 'o' || format == 'x' || format == 'X')
+		counter = printbase(format, ptr, counter);
+	else if (format == 'u')
+		counter = printunsign(ptr, counter);
+	else
+	{
+		_putchar('%');
+		_putchar(format);
+		counter += 2;
+	}
+	return (counter);
 }
-return (counter);
-}
+
