b3d3df820ddd75957a0b505e914aa16f1ae88df3
Nada <nadafarrag619@gmail.com>
2023-09-16 18:45:42 +0300

    printstringASCII.c

--- printstringASCII.c
+++ printstringASCII.c
@@ -9,18 +9,23 @@ int printstringASCII(va_list ptr, int counter)
 {
 char *str = va_arg(ptr, char*);
 if (str == NULL)
+{
 str = "(null)";
-for (int k = 0; str[k] != '\0'; k++)
+}
+for (int u = 0; str[u] != '\0'; u++)
 {
-if (str[k] < 32 || str[k] >= 127)
+if (str[u] < 32 || str[u] >= 127)
 {
-printf("\\x%02X", (unsigned char)str[k]);
+_putchar('\\');
+_putchar('x');
+_putchar((str[u] >> 4) + '0');
+_putchar((str[u] & 0xF) + ((str[u] & 0xF) < 10 ? '0' : 'A' - 10));
+counter += 4;
 }
 else
 {
-_putchar(str[k]);
-}
+_putchar(str[u]);
 counter++;
 }
-return (counter);
+}
 }
